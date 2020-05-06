dc450a5a3490f810bdccd3257096203af5c63fd5
Duy Hung Le <le.duy.hung@sun-asterisk.com>
2020-05-06 11:46:24 +0700

    dev header

--- frontend/src/components/Top/index.js
+++ frontend/src/components/Top/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+function Oxy(num) {
+  return (
+    <div className="oxy">
+      {/* <div className="bloc1" />
+      <div className="bloc2">{num.value}</div> */}
+    </div>
+  )
+}
+
+function Level(num) {
+  return (
+    <div className="lv">
+      Level {num.value}
+    </div>
+  )
+}
+
 function Top() {
-  return <div>Top</div>;
+  return <div>
+      <Oxy  value={"100k"} />
+      <Level  value={"3"} />
+  </div>;
 }
 
 export default Top;
